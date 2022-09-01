a9d258c3bcbf1b319a3766520233146d13d8cfc4
Ondra Chaloupka <chalda@chainkeepers.io>
2022-09-01 12:17:16 +0200

    Adding instruction deposit_stake_account

---
+++ libs/marinade-sdk/src/instructions/deposit_stake_account.rs
@@ -0,0 +1,46 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::pubkey::Pubkey;
+
+#[derive(
+    MarinadeInstructionData,
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    BorshSerialize,
+    BorshDeserialize,
+)]
+#[discriminator([110, 130, 115, 41, 164, 102, 2, 59])]
+pub struct DepositStakeAccountData {
+    pub validator_index: u32,
+}
+
+#[derive(MarinadeInstructionAccounts)]
+#[ownerid(crate::ID)]
+pub struct DepositStakeAccountAccounts {
+    #[account(mut)]
+    pub state: Pubkey,
+    #[account(mut)]
+    pub validator_list: Pubkey,
+    #[account(mut)]
+    pub stake_list: Pubkey,
+    #[account(mut)]
+    pub stake_account: Pubkey,
+    #[account(signer)]
+    pub stake_authority: Pubkey,
+    #[account(mut)]
+    pub duplication_flag: Pubkey,
+    #[account(mut, signer)]
+    pub rent_payer: Pubkey,
+    #[account(mut)]
+    pub msol_mint: Pubkey,
+    #[account(mut)]
+    pub mint_to: Pubkey,
+    pub msol_mint_authority: Pubkey,
+    pub clock: Pubkey,
+    pub rent: Pubkey,
+    pub system_program: Pubkey,
+    pub token_program: Pubkey,
+    pub stake_program: Pubkey,
+}
--- libs/marinade-sdk/src/instructions/mod.rs
+++ libs/marinade-sdk/src/instructions/mod.rs
@@ -1,3 +1,4 @@
 pub mod change_authority;
 pub mod config_lp;
+pub mod deposit_stake_account;
 pub mod initialize;
