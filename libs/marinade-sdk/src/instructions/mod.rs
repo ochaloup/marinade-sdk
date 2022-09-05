5c56a33c46aafc81d2894488c296a8c73e619d70
Ondra Chaloupka <chalda@chainkeepers.io>
2022-09-05 11:04:00 +0200

    Instruction remove_liquidity

--- libs/marinade-sdk/src/instructions/mod.rs
+++ libs/marinade-sdk/src/instructions/mod.rs
@@ -3,3 +3,4 @@ pub mod change_authority;
 pub mod config_lp;
 pub mod deposit_stake_account;
 pub mod initialize;
+pub mod remove_liquidity;
---
+++ libs/marinade-sdk/src/instructions/remove_liquidity.rs
@@ -0,0 +1,41 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::pubkey::Pubkey;
+
+#[derive(
+    MarinadeInstructionData,
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    BorshSerialize,
+    BorshDeserialize,
+)]
+#[discriminator([80, 85, 209, 72, 24, 206, 177, 108])]
+pub struct RemoveLiquidityData {
+    pub tokens: u64,
+}
+
+#[derive(MarinadeInstructionAccounts)]
+#[ownerid(crate::ID)]
+pub struct RemoveLiquidityAccounts {
+    #[account(mut)]
+    pub state: Pubkey,
+    #[account(mut)]
+    pub lp_mint: Pubkey,
+    #[account(mut)]
+    pub burn_from: Pubkey,
+    #[account(signer)]
+    pub burn_from_authority: Pubkey,
+    #[account(mut)]
+    pub transfer_sol_to: Pubkey,
+    #[account(mut)]
+    pub transfer_msol_to: Pubkey,
+    #[account(mut)]
+    pub liq_pool_sol_leg_pda: Pubkey,
+    #[account(mut)]
+    pub liq_pool_msol_leg: Pubkey,
+    pub liq_pool_msol_leg_authority: Pubkey,
+    pub system_program: Pubkey,
+    pub token_program: Pubkey,
+}
