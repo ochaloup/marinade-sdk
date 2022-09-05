0784fbe1acf81ec5e309f610501823d170eb3d73
Ondra Chaloupka <chalda@chainkeepers.io>
2022-09-05 13:23:53 +0200

    Instruction claim

---
+++ libs/marinade-sdk/src/instructions/claim.rs
@@ -0,0 +1,30 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::pubkey::Pubkey;
+
+#[derive(
+    MarinadeInstructionData,
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    BorshSerialize,
+    BorshDeserialize,
+)]
+#[discriminator([62, 198, 214, 193, 213, 159, 108, 210])]
+pub struct ClaimData {}
+
+#[derive(MarinadeInstructionAccounts)]
+#[ownerid(crate::ID)]
+pub struct ClaimAccounts {
+    #[account(mut)]
+    pub state: Pubkey,
+    #[account(mut)]
+    pub reserve_pda: Pubkey,
+    #[account(mut)]
+    pub ticket_account: Pubkey,
+    #[account(mut)]
+    pub transfer_sol_to: Pubkey,
+    pub clock: Pubkey,
+    pub system_program: Pubkey,
+}
--- libs/marinade-sdk/src/instructions/mod.rs
+++ libs/marinade-sdk/src/instructions/mod.rs
@@ -1,5 +1,6 @@
 pub mod add_liquidity;
 pub mod change_authority;
+pub mod claim;
 pub mod config_lp;
 pub mod deposit_stake_account;
 pub mod initialize;
