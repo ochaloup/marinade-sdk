ac515c41f86b8d368802ba3f6c44e18b2de757bc
Ondra Chaloupka <chalda@chainkeepers.io>
2022-09-05 14:13:23 +0200

    Instruction deactivate_stake

---
+++ libs/marinade-sdk/src/instructions/deactivate_stake.rs
@@ -0,0 +1,43 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::pubkey::Pubkey;
+
+#[derive(
+    MarinadeInstructionData,
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    BorshSerialize,
+    BorshDeserialize,
+)]
+#[discriminator([165, 158, 229, 97, 168, 220, 187, 225])]
+pub struct DeactivateStakeData {
+    pub stake_index: u32,
+    pub validator_index: u32,
+}
+
+#[derive(MarinadeInstructionAccounts)]
+#[ownerid(crate::ID)]
+pub struct DeactivateStakeAccounts {
+    #[account(mut)]
+    pub state: Pubkey,
+    pub reserve_pda: Pubkey,
+    #[account(mut)]
+    pub validator_list: Pubkey,
+    #[account(mut)]
+    pub stake_list: Pubkey,
+    #[account(mut)]
+    pub stake_account: Pubkey,
+    pub stake_deposit_authority: Pubkey,
+    #[account(mut, signer)]
+    pub split_stake_account: Pubkey,
+    #[account(mut, signer)]
+    pub split_stake_rent_payer: Pubkey,
+    pub clock: Pubkey,
+    pub rent: Pubkey,
+    pub epoch_schedule: Pubkey,
+    pub stake_history: Pubkey,
+    pub system_program: Pubkey,
+    pub stake_program: Pubkey,
+}
