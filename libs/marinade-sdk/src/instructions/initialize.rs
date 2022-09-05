63f6e2577d066c71669bcaf2063699a6e0166313
Ondra Chaloupka <chalda@chainkeepers.io>
2022-09-05 13:16:14 +0200

    Instruction initialize

--- libs/marinade-sdk/src/instructions/initialize.rs
+++ libs/marinade-sdk/src/instructions/initialize.rs
@@ -1,50 +1,70 @@
 use crate::state::fee::Fee;
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{account_info::AccountInfo, pubkey::Pubkey};
+use solana_program::pubkey::Pubkey;
 
-pub struct InitializeAccountInfos<'info> {
-    pub creator_authority: AccountInfo<'info>,
-    pub state: AccountInfo<'info>,
-
-    pub reserve_pda: AccountInfo<'info>,
-    pub stake_list: AccountInfo<'info>,
-    pub validator_list: AccountInfo<'info>,
-
-    pub msol_mint: AccountInfo<'info>,
-
-    pub operational_sol_account: AccountInfo<'info>,
-
-    pub liq_pool: LiqPoolInitializeAccountInfos<'info>,
-
-    pub treasury_msol_account: AccountInfo<'info>,
-
-    pub clock: AccountInfo<'info>,
-    pub rent: AccountInfo<'info>,
-}
-
-pub struct LiqPoolInitializeAccountInfos<'info> {
-    pub lp_mint: AccountInfo<'info>,
-    pub sol_leg_pda: AccountInfo<'info>,
-    pub msol_leg: AccountInfo<'info>,
-}
-
-#[derive(Clone, Copy, Debug, Default, PartialEq, BorshSerialize, BorshDeserialize)]
+#[derive(
+    MarinadeInstructionData,
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    BorshSerialize,
+    BorshDeserialize,
+)]
+#[discriminator([175, 175, 109, 31, 13, 152, 155, 237])]
 pub struct InitializeData {
     pub admin_authority: Pubkey,
     pub validator_manager_authority: Pubkey,
     pub min_stake: u64,
     pub reward_fee: Fee,
-
     pub liq_pool: LiqPoolInitializeData,
     pub additional_stake_record_space: u32,
     pub additional_validator_record_space: u32,
     pub slots_for_stake_delta: u64,
 }
 
-#[derive(Clone, Copy, Debug, Default, PartialEq, BorshSerialize, BorshDeserialize)]
+#[derive(
+    MarinadeInstructionData,
+    Clone,
+    Copy,
+    Debug,
+    Default,
+    PartialEq,
+    BorshSerialize,
+    BorshDeserialize,
+)]
+#[discriminator([1,2,3,4,5,6,7,8])] // fake discriminator
 pub struct LiqPoolInitializeData {
     pub lp_liquidity_target: u64,
     pub lp_max_fee: Fee,
     pub lp_min_fee: Fee,
     pub lp_treasury_cut: Fee,
+}
+
+#[derive(MarinadeInstructionAccounts)]
+#[ownerid(crate::ID)]
+pub struct InitializeAccounts {
+    #[account(signer)]
+    pub creator_authority: Pubkey,
+    pub marinade: Pubkey, // state
+    pub reserve_pda: Pubkey,
+    #[account(mut)]
+    pub stake_list: Pubkey,
+    #[account(mut)]
+    pub validator_list: Pubkey,
+    pub msol_mint: Pubkey,
+    pub operational_sol_account: Pubkey,
+    pub liq_pool: LiqPoolInitializeAccounts,
+    pub treasury_msol_account: Pubkey,
+    pub clock: Pubkey,
+    pub rent: Pubkey,
+}
+
+#[derive(MarinadeInstructionAccounts)]
+#[ownerid(crate::ID)]
+pub struct LiqPoolInitializeAccounts {
+    pub lp_mint: Pubkey,
+    pub sol_leg_pda: Pubkey,
+    pub msol_leg: Pubkey,
 }
